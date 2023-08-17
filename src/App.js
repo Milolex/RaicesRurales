ca4472443209b3605eb1d0f72c62f669b6891865
Camilo Martínez <kmilomartinezkam@gmail.com>
2023-08-16 19:00:31 -0500

    Inicio Del Desarrollo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/Inicio/navBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Inicio/navBar.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import "../../assets/css/navbar.css"
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div className="nav">
+                <nav className="navbar">
+                    <ul className="navLis">
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Rutas</a></li>
+                        <li><a href='#'>Contactos</a></li>
+                        <li><a href='#'>Login</a></li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
+
+
+NavBar.propTypes = {
+
+};
+
+
+export default NavBar;
