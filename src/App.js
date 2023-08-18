7b3c1dac3f6494bc80c19025ca1a4f1f04041353
wills <willslipknotpsico@gmail.com>
2023-08-18 10:09:14 -0500

    Prueba

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import NavBar from './components/Inicio/navBar';
 import Encabezado from './components/Inicio/encabezado';
 import Promo from './components/Inicio/promo';
-
+//prueba
+//prueba 2
 function App() {
   return (
     <div className="App">
