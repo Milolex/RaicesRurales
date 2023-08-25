ddf849b3484e75394487b1891358a7564b914ed2
Roger Acosta <roger-santiago12@outlook.com>
2023-08-24 20:59:45 -0500

    avance_target

--- src/components/Inicio/target.jsx
+++ src/components/Inicio/target.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import imagen_t from "../../assets/images/caminar.png"
+import image from "../../assets/images/caminar.png"
+import imagen_t from "../../assets/images/target_p.jpeg"
+
 import "../../assets/css/target.css" 
 
 class Target extends Component {
     render() {
         return (
-            <div className='target_ruta'>
-                <h1><img src={imagen_t}/>Paseo turistico+estadia</h1>
+            <div className='contenedor_target'>
+
+                <div className='target_ruta'>
+
+                    <img className= 'image_icon' src={image} />
+
+                    <h1 className='titulo_tar'>Paseo turistico+estadia</h1>
+                </div>
+                
+                <div className='imagen_target'>
+
+                    <img className= 'image_tar' src={imagen_t} />
+
+                </div>
+
+                <div className="texto_sp">
+
+                    <h1 className='txt_sup'> Raices Rurales </h1>
+
+                </div>
+                    
                
+
             </div>
+
         );
     }
 }
